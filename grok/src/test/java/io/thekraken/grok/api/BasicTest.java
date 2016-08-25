f62600e1fd97d8b6c9fa7723eff0cb80f432abe0
Yu Shengqiang <shefron2012@hotmail.com>
2016-08-25 08:55:16 +0800

    add 0.1.5-SNAPSHOT

---
+++ grok/src/test/java/io/thekraken/grok/api/BasicTest.java
@@ -0,0 +1,98 @@
+package io.thekraken.grok.api;
+
+import oi.thekraken.grok.api.Grok;
+import oi.thekraken.grok.api.exception.GrokException;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BasicTest {
+
+    @Test
+    public void test001_compileFailOnInvalidExpression() throws GrokException {
+
+        Grok g = Grok.create(ResourceManager.PATTERNS, null);
+
+        List<String> badRegxp = new ArrayList<String>();
+        badRegxp.add("[");
+        badRegxp.add("[foo");
+        badRegxp.add("?");
+        badRegxp.add("foo????");
+        badRegxp.add("(?-");
+
+        boolean thrown = false;
+
+        /** This should always throw */
+        for (String regx : badRegxp) {
+            try {
+                g.compile(regx);
+            } catch (PatternSyntaxException e) {
+                thrown = true;
+            }
+            assertTrue(thrown);
+            thrown = false;
+        }
+    }
+
+    @Test
+    public void test002_compileSuccessValidExpression() throws GrokException {
+
+        Grok g = Grok.create(ResourceManager.PATTERNS);
+
+        List<String> regxp = new ArrayList<String>();
+        regxp.add("[hello]");
+        regxp.add("(test)");
+        regxp.add("(?:hello)");
+        regxp.add("(?=testing)");
+
+        for (String regx : regxp) {
+            g.compile(regx);
+        }
+    }
+
+    @Test
+    public void test003_samePattern() throws GrokException {
+        Grok g = Grok.create(ResourceManager.PATTERNS);
+
+        String pattern = "Hello World";
+        g.compile(pattern);
+        assertEquals(pattern, g.getOriginalGrokPattern());
+    }
+
+    @Test
+    public void test004_sameExpantedPatern() throws GrokException {
+        Grok g = Grok.create(ResourceManager.PATTERNS);
+
+        g.addPattern("test", "hello world");
+        g.compile("%{test}");
+        assertEquals("(?<name0>hello world)", g.getNamedRegex());
+    }
+
+    @Test
+    public void test005_testLoadPatternFromFile() throws IOException, GrokException {
+        File temp = File.createTempFile("grok-tmp-pattern", ".tmp");
+        getClass();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+        bw.write("TEST \\d+");
+        bw.close();
+
+        Grok grok = Grok.create(temp.getAbsolutePath());
+        grok.compile("%{TEST}");
+        assertEquals("(?<name0>\\d+)", grok.getNamedRegex());
+        temp.delete();
+    }
+
+}
---
+++ grok/src/test/java/io/thekraken/grok/api/MessagesTest.java
@@ -0,0 +1,38 @@
+package io.thekraken.grok.api;
+
+import oi.thekraken.grok.api.Grok;
+import oi.thekraken.grok.api.Match;
+import oi.thekraken.grok.api.exception.GrokException;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+public class MessagesTest {
+
+    public final static String LOG_FILE = ResourceManager.MESSAGES;
+
+    @Test
+    public void test001_linux_messages() throws GrokException, IOException {
+        Grok g = new Grok();
+        g.addPatternFromFile(ResourceManager.PATTERNS);
+        g.compile("%{MESSAGESLOG}");
+
+        BufferedReader br = new BufferedReader(new FileReader(LOG_FILE));
+        String line;
+        System.out.println("Starting test with linux messages log -- may take a while");
+        while ((line = br.readLine()) != null) {
+            Match gm = g.match(line);
+            gm.captures();
+            assertNotNull(gm.toJson());
+            assertNotEquals("{\"Error\":\"Error\"}", gm.toJson());
+        }
+        br.close();
+    }
+
+}
